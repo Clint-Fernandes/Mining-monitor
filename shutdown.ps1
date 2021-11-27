89e52f9ede9625c56ed1de7576089eda0ea77047
Clint Fernandes <clint.fernandes@turbocam.com>
2021-11-27 12:19:06 +0530

    Initial Commit

--- shutdown.ps1
+++
@@ -1,8 +0,0 @@
-﻿Start-Sleep -Seconds 3
-#Test-Connection "10.200.200.99"
-
-if(Test-Connection "10.200.200.999" -Count 1 -TimeToLive 135) {
-   Write-Output "hello"
-} else {
-    Write-Output "bye"
-}
